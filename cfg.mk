4c09ea0df417a948ded14ca890d34405c02347f6
Jim Meyering <meyering@fb.com>
2018-01-07 14:33:54 -0800

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -35,7 +35,7 @@ bootstrap-tools = autoconf,automake,gnulib
 # Now that we have better tests, make this the default.
 export VERBOSE = yes
 
-old_NEWS_hash = 143a1e2047fa15579bd66c75c7d962ce
+old_NEWS_hash = be971f82131abd0bf8f988ec98ebc4e5
 
 sc_obs_header_regex = \
   \<(STDC_HEADERS|HAVE_(LIMITS|STRING|UNISTD|STDLIB)_H)\>
